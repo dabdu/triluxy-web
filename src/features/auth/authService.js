0d36d2d6b9958a0ed68e238ebbf77c4f70f7063f
Ibrahim Dauda <dabdulxyz@gmail.com>
2022-08-26 02:56:17 -0700

    Headers Defined

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 const API_URL = "/user/";
+const config = {
+  headers: {
+    "Content-Type":"Application/json"  },
+};
 
 // Register User
 
@@ -12,9 +16,8 @@ const register = async (userData) => {
   return response.data;
 };
 // Login User
-
 const login = async (userData) => {
-  const response = await axios.post(`${API_URL}login`, userData);
+  const response = await axios.post(`${API_URL}login`, userData, config);
   if (response.data) {
     localStorage.setItem("user", JSON.stringify(response.data));
   }
